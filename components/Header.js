1417132f772b4b78b1816b51ba35e9222e1b6278
RYA234 <mryuabc@gmail.com>
2023-05-20 10:53:18 +0900

    39  Add Search bar

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import {SearchIcon} from "@heroicons/react/outline"
 
 
 export default function Header(){
@@ -20,6 +21,12 @@ export default function Header(){
                     className="object-contain"
                     alt = ""
                 />
+            </div>
+            <div className="relative mt-1">
+                <div className="absolute top-2 left-2">
+                    <SearchIcon className="h-5 text-gray-500" />
+                </div>
+                <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus-ring-black focus-border-black rounded-md"/>
             </div>
             <h1>Right sides</h1>
         </div>
