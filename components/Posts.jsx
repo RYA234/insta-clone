b257152058de4cf87507830903cc2f43b749e28f
RYA234 <mryuabc@gmail.com>
2023-05-20 17:42:29 +0900

    45 create the header and image sections of the post component

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -5,7 +5,7 @@ const posts =  [
     {
         id: "1",
         username: "codewithsahand",
-        userImg: "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
+        userImg: "https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/2710623/profile-images/1655538932",
         img: "https://images.unsplash.com/photo-1643818507403-a3ed10760d16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         caption: "Nice picture"
 
@@ -13,7 +13,7 @@ const posts =  [
     {
         id: "2",
         username: "ghavidelsahand",
-        userImg: "https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048",
+        userImg: "https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/2710623/profile-images/1655538932",
         img: "https://images.unsplash.com/photo-1643806720662-f9bc01be4e83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
         caption: "New picture from my city"
 
