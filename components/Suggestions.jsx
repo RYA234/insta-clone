d837fad537cad0cf80be4e0a77aebec02fa042d1
RYA234 <mryuabc@gmail.com>
2023-07-10 23:12:15 +0900

    48 build the suggestion part of the feed component

---
+++ components/Suggestions.jsx
@@ -0,0 +1,32 @@
+
+import { useEffect,useState } from "react";
+
+export default function Suggestions(){
+    const [suggestions,setSuggestions] = useState([]);
+    useEffect(() =>{
+        (async () => {
+            const randomUsersResponse = await fetch(
+                "https://randomuser.me/api/?results=5&inc=name,login,picture,location"
+              ).then((res) => res.json());
+              setSuggestions(randomUsersResponse.results)
+            })();    
+        },[]);
+    return(
+        <div className="mt-4 ml-10">
+            <div className="flex justify-between mb-5 text-sm">
+                <h3 className="font-bold text-gray-400">Suggestion for you</h3>
+                <button className="text-gray-600 font-semibold">See all</button>
+            </div>
+            {suggestions.map(suggestion => (
+                <div className="flex items-center justify-between mt-3">
+                    <img className="h-10 rounded-full border p-[2px]"  src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`} alt="" />
+                    <div className="flex-1 ml-4">
+                        <h2 className="font-semibold text-sm">{suggestion.login.username}</h2>
+                        <h3 className="text-sm text-gray-400 truncate w-[230px]">{suggestion.location.country}</h3>
+                    </div>
+                    <button className="font-semibold text-blue-400 text-sm">Follow</button>
+                </div>
+            ))}
+        </div>
+    )
+}
