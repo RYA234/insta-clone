7cef48a27bd1486745674da426249706aa1e9350
RYA234 <mryuabc@gmail.com>
2023-07-12 12:03:35 +0900

    53 modify mini profile and story components to include the session

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -1,18 +1,23 @@
 
+import {useSession,signOut} from "next-auth/react";
+
 export default function MiniProfile(){
+    const {data:session} = useSession();
     return(
         <div className="">
             <img 
                 className="h-16 rounded-full border p-[2px]"
-                src="https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/2710623/profile-images/1655538932"
+                src={session?.user.image}
                 alt="user-image"
             />
             
             <div className="flex-1 ml-4">
-                <h2 className="font-bold">RYA234</h2>
+                <h2 className="font-bold">{session?.user.username}</h2>
                 <h3 className="text-sm text-gray-400">Welcome to instagram</h3>
             </div>
-            <button className="font-semibold text-blue-400 text-sm">Sign out</button>
+            <button onClick={signOut} className="font-semibold text-blue-400 text-sm">
+                Sign out
+            </button>
         </div>
     )
 }
--- components/Stories.jsx
+++ components/Stories.jsx
@@ -3,11 +3,12 @@
 import {useEffect,useState} from "react";
 import Story from "./Story";
 import { UserCircleIcon } from "@heroicons/react/outline";
+import{useSession} from "next-auth/react";
 
 
 export default  function Stories(){
     const[storyUsers,setStoryUsers] = useState([]);
-    
+    const{data:session} = useSession();
     useEffect(() =>{
         (async () => {
             const randomUsersResponse = await fetch(
@@ -30,6 +31,9 @@ export default  function Stories(){
 
       return(
         <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
+          {session && (
+            <Story img={session.user.image} username={session.user.username} isUser="true"/>
+          )}
             {storyUsers.map((user) => (
                 <Story key={user.uuid} username={user.login.username} img={user.picture.medium } />
             ))}
